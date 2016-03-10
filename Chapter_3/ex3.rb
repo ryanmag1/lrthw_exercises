32e7d1e91da0a354823e38788ffe7dcf002d6cb8
Ryan Magee <ryanmag1@yahoo.com>
2016-03-09 22:57:20 -0700

    Completed the exersizes in ch3 of lrthw and created the files requested to demonstrate the output
    [#113796247]

---
+++ Chapter_3/ex3.rb
@@ -0,0 +1,31 @@
+# prints "I will now count my chickens"
+puts "I will now count my chickens:"
+
+# Addes 25 to 30, divides by 6 ()for the hens)
+# For the roosters subtracts 25 from 100 and multiplies it by 4 modulus 4
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# prints text
+puts "Now I will count the eggs:"
+
+# adds, subtracts, then adds modulus and subtracts a division
+
+puts 3 + 2 + 1 - 5+ 4 % 2 - 1 / 4 + 6
+
+# asks if 5 is greater than negative 2
+puts "Is it true that 3 + 2 < 5 - 7?"
+puts 3 + 2 < 5 - 7
+# proves that the question above is false by doing the math
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+# asks if 5 is greater that -2.  Returns true
+puts "Is it greater? #{5 > -2}"
+# Asks if 5 is greater than or equal to -2, Returns true
+puts "Is it greater or equal? #{5 >= -2}"
+# Asks if 5 is less than or equal to -2, returns false.
+puts "Is it less or equal? #{5 <= -2}"
---
+++ Chapter_3/ex3_floating.rb
@@ -0,0 +1,31 @@
+# prints "I will now count my chickens"
+puts "I will now count my chickens:"
+
+# Addes 25 to 30, divides by 6 ()for the hens)
+# For the roosters subtracts 25 from 100 and multiplies it by 4 modulus 4
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# prints text
+puts "Now I will count the eggs:"
+
+# adds, subtracts, then adds modulus and subtracts a division
+
+puts 3.0 + 2.0 + 1.0 - 5.0+ 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# asks if 5 is greater than negative 2
+puts "Is it true that 3 + 2 < 5 - 7?"
+puts 3.0 + 2.0 < 5.0 - 7.0
+# proves that the question above is false by doing the math
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+# asks if 5 is greater that -2.  Returns true
+puts "Is it greater? #{5.0 > -2.0}"
+# Asks if 5 is greater than or equal to -2, Returns true
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# Asks if 5 is less than or equal to -2, returns false.
+puts "Is it less or equal? #{5.0 <= -2.0}"
